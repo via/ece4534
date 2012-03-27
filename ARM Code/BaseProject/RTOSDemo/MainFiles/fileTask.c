45a18f40787f87ce8dcbd79808a576885b08e5f3
Dizzeh <notsoinsightful@gmail.com>
2012-03-26 20:54:50 -0400

    forgot some files

---
+++ ARM Code/BaseProject/RTOSDemo/MainFiles/fileTask.c
@@ -0,0 +1,188 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/* Scheduler include files. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* include files. */
+#include "vtUtilities.h"
+#include "fileTask.h"
+#include "lcdtask.h"
+#include "diskio.h"
+#include "ff.h"
+
+// I have set this to a large stack size because of (a) using ////printf() and (b) the depth of function calls
+#if printf_VERSION==1
+#define fileSTACK_SIZE		8*configMINIMAL_STACK_SIZE
+#else
+#define fileSTACK_SIZE		4*configMINIMAL_STACK_SIZE
+#endif
+
+// Set the task up to run every 200 ms
+#define taskRUN_RATE	( ( portTickType ) 200 )
+
+static portTASK_FUNCTION_PROTO( vFileTask, pvParameters );
+static FRESULT F_Write(BYTE Msg[], UINT msg_size, TCHAR *path, int append);
+static FRESULT F_Read (BYTE Buf[], UINT buf_size, TCHAR *path);
+
+/*-----------------------------------------------------------*/
+
+void vStartFileTask( unsigned portBASE_TYPE uxPriority,vtFileStruct *ptr )
+{
+
+	// Create the queue that will be used to talk to the LCD
+	if ((ptr->inQ = xQueueCreate(vtFileQLen,sizeof(vtFileMsg))) == NULL) {
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	/* Start the task */
+	portBASE_TYPE retval;
+	if ((retval = xTaskCreate( vFileTask, ( signed char * ) "File", fileSTACK_SIZE, (void*)ptr, uxPriority, ( xTaskHandle * ) NULL )) != pdPASS) {
+		VT_HANDLE_FATAL_ERROR(retval);
+	}
+}
+
+static FATFS fatfs;  // Filesystem object
+static FIL fil;   	 // File object
+
+static portTASK_FUNCTION( vFileTask, pvParameters )
+{
+	portTickType xUpdateRate, xLastUpdateTime;
+	vtFileMsg msgBuffer;
+	vtFileStruct *filePtr = (vtFileStruct *) pvParameters;
+	vtLCDStruct *lcdData = filePtr->lcdData;
+	vtLCDMsg lcdBuffer;
+	
+
+    static BYTE buf[64];
+    BYTE Message[] = "Test message\n" ; // message's content
+    TCHAR *FilePath = "0:message.txt" ; // file path
+	
+	// Scale the update rate to ensure it really is in ms
+	xUpdateRate = taskRUN_RATE / portTICK_RATE_MS;
+	/* We need to initialise xLastUpdateTime prior to the first call to 
+	vTaskDelayUntil(). */
+	xLastUpdateTime = xTaskGetTickCount();
+	// Note that srand() & rand() require the use of malloc() and should not be used unless you are using
+	//   MALLOC_VERSION==1
+	#if MALLOC_VERSION==1
+	srand((unsigned) 55); // initialize the random number generator to the same seed for repeatability
+	#endif
+	
+	// === MOUNT ===
+	f_mount(0, &fatfs);  /* Register volume work area (never fails) */
+	
+	// Infinite task loop should probably go here later
+	FRESULT rc; 
+    rc = F_Write(Message, sizeof(Message) - 1, FilePath, 1);
+    rc = F_Read(buf, sizeof(buf), FilePath);
+	
+	sprintf((char*)(lcdBuffer.buf), buf);
+	lcdBuffer.length = strlen((char*)(lcdBuffer.buf)) + 1;
+	if(xQueueSend(lcdData->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE){
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+
+	// === UMOUNT ===
+    f_mount(0, NULL);/* Unregister work area prior to discard it */
+	
+    //------------------------------------
+    // Like all good tasks, this should never exit
+	for(;;)
+	{	
+		/* Ask the RTOS to delay reschduling this task for the specified time */
+		vTaskDelayUntil( &xLastUpdateTime, xUpdateRate );
+
+		if (xQueueReceive(filePtr->inQ,(void *) &msgBuffer,portMAX_DELAY) != pdTRUE) {
+			VT_HANDLE_FATAL_ERROR(0);
+		}
+		//Log that we are processing a message
+		vtITMu8(vtITMPortLCDMsg,msgBuffer.length);
+		// Do Stuff Here
+	}
+    //vTaskDelete(NULL);
+} 
+
+FRESULT F_Write(BYTE Message[], UINT msg_size, TCHAR *filepath, int append){
+	FRESULT rc;
+	UINT bw = 0;
+	
+	
+	
+	// === Write test ===
+    rc = f_open(&fil, filepath, FA_WRITE | FA_OPEN_ALWAYS);
+    if(rc){
+		//printf("File open error = %u",rc);
+    }
+	
+	// Seek end if append
+	if(append){
+		rc = f_lseek(&fil, f_size(&fil));
+	}
+
+    rc = f_write(&fil, Message, msg_size, &bw); // write file
+    if(rc){
+		//printf("Write error = %u", rc);
+    }
+
+    rc = f_close(&fil);
+    if(rc){
+		//printf("File close error = %u", rc);
+    }
+	
+	return rc;
+}
+
+FRESULT F_Read(BYTE Buf[], UINT buf_size, TCHAR *filepath){
+	FRESULT rc;
+	UINT numread = 0;
+	UINT br = 0;
+	
+	// === Read test ===
+    rc = f_open(&fil, filepath, FA_READ | FA_OPEN_EXISTING);
+    if(rc){
+		//printf("File open error = %u", rc);
+    }
+
+    for(;;){
+		rc = f_read(&fil, Buf, buf_size, &br);	// Read chunk of file
+		if(rc || !br){
+			break;         							// Error or EOF
+		}
+		int i = 0;
+		for(; i < br; i++){
+			putchar(Buf[i]);
+		}
+		numread += br;
+    }
+    if(rc){
+		//printf("Read error = %u", rc);
+	}
+
+    rc = f_close(&fil);
+    if(rc){
+		//printf("File close error = %u", rc);
+    }	
+	
+	return rc;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
