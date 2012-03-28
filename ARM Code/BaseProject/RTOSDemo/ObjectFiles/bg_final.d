4aa497c9c6aaa4e52441d4a6dc879ab35bef7782
unknown <csbarcode@gmail.com>
2012-03-27 20:00:55 -0400

    removing object files

--- ARM Code/BaseProject/RTOSDemo/ObjectFiles/bg_final.d
+++
@@ -1,1 +0,0 @@
-ObjectFiles/bg_final.o: ../vtCode/vtLCD/Bg_final.c
--- ARM Code/BaseProject/RTOSDemo/ObjectFiles/lpc17xx_libcfg_default.d
+++
@@ -1,6 +0,0 @@
-ObjectFiles/lpc17xx_libcfg_default.o: \
- ../NXPDrivers/source/lpc17xx_libcfg_default.c \
- ../NXPDrivers/include/lpc17xx_libcfg_default.h \
- ../NXPDrivers/include/lpc_types.h \
- c:\program\ files\codesourcery\sourcery\ g++\ lite\bin\../lib/gcc/arm-none-eabi/4.5.2/include/stdint.h \
- c:\program\ files\codesourcery\sourcery\ g++\ lite\bin\../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/stdint.h
